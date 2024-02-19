411be0532294ed4bb4318e6fc62d3f4f9e913cc4
ccirelli2 <ccirelli2@gmail.com>
2024-02-19 17:19:30 -0500

    setup

--- examples/example_majority_vote.py
+++ examples/example_majority_vote.py
@@ -7,9 +7,16 @@ import git
 from uuid import uuid4
 import pandas as pd
 import numpy as np
+
+# Data
+from sklearn.datasets import make_classification, make_regression
+
+# Processing
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+
+# Modeling
 from lightgbm import LGBMClassifier, LGBMRegressor
 
 # Project
@@ -33,7 +40,7 @@ class MajorityVoteFeatureImportance:
     Class object to measure feature important using majority voting across n-estimators.
 
     ::objective: str value.  The objective of the model.  Either 'classification' or 'regression'.
-    ::threshold: float value > 0 and < 1.  The threshold value is used to select the most important features by model.
+    ::vote_threshold: float value > 0 and < 1.  The threshold value is used to select the most important features by model.
         If a feature is important it is assigned a one.
     ::estimators: list of estimators to use for majority voting.  All estimators must implement fit and
         predict methods.  All estimators will be called using the default parameter values.
@@ -42,11 +49,16 @@ class MajorityVoteFeatureImportance:
     def __init__(
         self,
         objective: str = "classification",
-        threshold: float = 0.5,
+        data: pd.DataFrame = pd.DataFrame(),
+        target_column: str = "TARGET",
+        vote_threshold: float = 0.5,
         estimators: list = None,
     ):
         self.objective = objective
-        self.threshold = threshold
+        self.data = data
+        self.samples_synthetic = 10_000
+        self.target_column = target_column
+        self.vote_threshold = vote_threshold
         self.estimators = estimators
         assert self.objective in (
             "classification",
@@ -56,9 +68,37 @@ class MajorityVoteFeatureImportance:
             f"Class object {self.__class__.__name__} instantiated successfully with the following parameters:"
         )
         logger.info(
-            f"\t objective: {self.objective}, threshold: {self.threshold}, estimators: {self.estimators}"
+            f"\t objective: {self.objective}, threshold: {self.vote_threshold}, estimators: {self.estimators}"
         )
+
+    def _generate_data(self):
+        if self.data.empty:
+            logger.info("No data was provided.  Generating synthetic data.")
+            if self.objective == "classification":
+                X, y = make_classification(n_samples=self.samples_synthetic)
+                data = pd.DataFrame(
+                    X, columns=[f"feature_{i}" for i in range(X.shape[1])]
+                )
+                data[self.target_column] = y
+                logger.info(
+                    f"Data shape: {data.shape}, X shape: {X.shape}, y shape: {y.shape}"
+                )
+                self.data = data
+            else:
+                X, y = make_regression(n_samples=self.samples_synthetic)
+                data = pd.DataFrame(
+                    X, columns=[f"feature_{i}" for i in range(X.shape[1])]
+                )
+                data[self.target_column] = y
+                self.data = data
+        return self
+
+    def fit(self):
+        logger.info("Fitting the model.")
+        self._generate_data()
+        return self
 
 
 if __name__ == "__main__":
     mvfi = MajorityVoteFeatureImportance()
+    mvfi.fit()
