7d5189d09157d566c711b5320659bfc1372f4cbc
ccirelli2 <ccirelli2@gmail.com>
2024-02-27 06:28:39 -0500

    ...

--- examples/example_feature_imp_classification_user_data.py
+++ examples/example_feature_imp_classification_user_data.py
@@ -20,20 +20,17 @@ df = pd.read_csv(os.path.join(DIR_DATA_EXAMPLES, "synthetic_classification.csv")
 feature_set = [col for col in df.columns if col != "y"]
 categorical_features = feature_set[int(0.5 * len(feature_set)) :]
 numeric_features = [col for col in feature_set if col not in categorical_features]
-print(feature_set)
-
 
 # Instantiate our class object
 fp = FeatureImportanceClassification(
+    generate_synthetic_data=True,
     data=df,
     target_column="y",
     feature_set=feature_set,
     categorical_features=categorical_features,
     numeric_features=numeric_features,
-    generate_synthetic_data=False,
     plot_importance=False,
     test_size=0.33,
-    num_samples_synthetic=1000,
     estimators=(
         ("RandomForestClassifier", RandomForestClassifier()),
         ("LGBMClassifier", LGBMClassifier()),
